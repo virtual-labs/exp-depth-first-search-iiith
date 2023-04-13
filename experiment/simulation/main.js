2c80825623f0b95f595aef48f5f3cd7febaf61b3
Teja Pudi <saiteja11.pudi@gmail.com>
2023-04-14 01:44:43 +0530

    Edit:T

---
+++ experiment/simulation/main.js
@@ -0,0 +1,65 @@
+var cwidth = canv.offsetWidth;
+var cheight = canv.offsetHeight;
+
+console.log(cwidth, cheight);
+
+function preset() {
+    type = false;
+    nodes.push([1/2 * cwidth, 1/4 * cheight], [1/4 * cwidth, 1/2 * cheight], [3/4 * cwidth, 1/2 * cheight], [1/8 * cwidth, 3/4 * cheight], [3/8 * cwidth, 3/4 * cheight], [5/8 * cwidth, 3/4 * cheight], [7/8 * cwidth, 3/4 * cheight]);
+    edges.push([1, 2], [0, 3, 4], [0, 5, 6], [1], [1], [2], [2]);
+    for (n = 1; n <= nodes.length; n++) { 
+        exist.push(true);
+    }
+}
+function refresh() {
+    canv.width = canv.offsetWidth;
+    canv.height = canv.offsetHeight;
+    if (type) {
+        force();
+    }
+    drawField();
+}
+
+var visit = [];
+var prev = null;
+var prevprev = null;
+
+function drawiter(e) {
+    visiting = e;
+    visiting_edge = [prev, e];
+    visited.push(prev);
+    visited_edge.push([prevprev, prev]);
+    refresh();
+
+    prevprev = prev;
+    prev = visiting;
+}
+
+function DFS() {
+    e = visit.pop();
+    drawiter(e);
+
+    for (const next of edges[e].slice().reverse()) {
+        if (!visited.includes(next)) {
+            visit.push(next);
+        }
+    }
+}
+
+function BFS() {
+    e = visit.shift();
+    drawiter(e);
+
+    for (const next of edges[e]) {
+        if (!visited.includes(next)) {
+            visit.push(next);
+        }
+    }
+}
+
+var node_info;
+var edge_info;
+var status;
+
+preset();
+refresh();
